fb5b494c355eab6d6cc2e750bbc949d955faaf7b
zqijun <540173107@qq.com>
2021-06-10 10:39:46 +0800

    e

--- ws/message.go
+++ ws/message.go
@@ -1,16 +1,41 @@
 package ws
 
+import "encoding/json"
+
 const (
 	MessageTypeString = 0
 )
 
+type MessageData struct {
+	MessageType int
+	Data interface{}
+}
+
+func (msgData *MessageData) ToJson() ([]byte, error) {
+	return json.Marshal(msgData)
+}
+
+func (msgData *MessageData) ToMessage(from, to string) *Message {
+	return &Message{
+		From:        from,
+		To:          to,
+		MessageType: msgData.MessageType,
+		Data:        msgData.Data,
+	}
+}
+
 type Message struct {
-	Form        string
+	From        string
 	To          string
 	MessageType int
 	Data        interface{}
 }
 
-func NewMessageByBytes(b []byte) (*Message, error) {
-	return nil, nil
+func NewMessageByJsonBytes(b []byte, from, to string) (*Message, error) {
+	msgData := &MessageData{}
+	err := json.Unmarshal(b, msgData)
+	if err != nil {
+		return nil, err
+	}
+	return msgData.ToMessage(from, to), nil
 }
