eacbd532653aef016ebcd550e8132f50d9d389d0
zqijun <540173107@qq.com>
2021-06-10 10:58:01 +0800

    e

--- ws/message.go
+++ ws/message.go
@@ -6,36 +6,24 @@ const (
 	MessageTypeString = 0
 )
 
-type MessageData struct {
-	MessageType int
-	Data interface{}
-}
-
-func (msgData *MessageData) ToJson() ([]byte, error) {
-	return json.Marshal(msgData)
-}
-
-func (msgData *MessageData) ToMessage(from, to string) *Message {
-	return &Message{
-		From:        from,
-		To:          to,
-		MessageType: msgData.MessageType,
-		Data:        msgData.Data,
-	}
-}
-
 type Message struct {
-	From        string
-	To          string
-	MessageType int
-	Data        interface{}
+	From        string      `json:"from"`
+	To          string      `json:"to"`
+	MessageType int         `json:"message_type"`
+	Data        interface{} `json:"data"`
 }
 
 func NewMessageByJsonBytes(b []byte, from, to string) (*Message, error) {
-	msgData := &MessageData{}
-	err := json.Unmarshal(b, msgData)
+	msg := &Message{}
+	err := json.Unmarshal(b, msg)
 	if err != nil {
 		return nil, err
 	}
-	return msgData.ToMessage(from, to), nil
+	if from != "" {
+		msg.From = from
+	}
+	if to != "" {
+		msg.To = to
+	}
+	return msg, nil
 }
